ff45a235ea7a2050732aeac4ab383c370c23213a
Slivre <66972691+Slivre@users.noreply.github.com>
2024-03-19 15:31:12 -0400

    isGrounded Added to cocky

--- Assets/Scripts/CockyMove.cs
+++ Assets/Scripts/CockyMove.cs
@@ -6,6 +6,14 @@ public class CockyMove : MonoBehaviour
 {
     Rigidbody rb;
     public float Speed;
+
+    public float GroundCheckRadius;
+    public Vector3 GroundCheckOffset;
+    Vector3 GroundCheckDrawPos;
+
+    public LayerMask obsticalLayer;
+
+    public bool isGrounded;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,6 +23,21 @@ public class CockyMove : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        rb.velocity = transform.forward * Speed;
+
+        GroundCheckDrawPos = transform.position + GroundCheckOffset;
+        Collider[] colliders = Physics.OverlapSphere(GroundCheckDrawPos, GroundCheckRadius, obsticalLayer);
+        isGrounded = colliders.Length > 0 ? true : false;
+
+        if(isGrounded)
+        {
+            Debug.Log("Move");
+            rb.velocity = transform.right * Speed;
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        
+        Gizmos.DrawWireSphere(GroundCheckDrawPos, GroundCheckRadius);
     }
 }
